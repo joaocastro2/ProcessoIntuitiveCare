f13173ce08e3e9c8bf031474857dcf7a84d03c57
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-29 03:50:11 -0300

    ADD: Clase que pega os arquivos pdf e comprimem em um unico .rar

---
+++ TesteIntuitiveCare/WebScraping/src/main/java/com/IntuitiveCare/WebScraping/Service/FileCompressionService.java
@@ -0,0 +1,46 @@
+package com.IntuitiveCare.WebScraping.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Service
+public class FileCompressionService {
+
+
+     //Compacta uma lista de arquivos em um único arquivo ZIP.
+    public void comprimirArquivos(String[] filePaths, String outputZipPath) {
+        try (FileOutputStream fos = new FileOutputStream(outputZipPath);
+             ZipOutputStream zipOut = new ZipOutputStream(fos)) {
+
+            // Itera por todos os arquivos fornecidos e adiciona ao ZIP
+            for (String arquivo : filePaths) {
+                File arquivoparazip = new File(arquivo);
+
+                // Verifica se o arquivo existe antes de tentar compactar
+                if (!arquivoparazip.exists()) {
+                    System.out.println("Arquivo não encontrado: " + arquivo);
+                    continue;
+                }
+
+                // Adiciona o arquivo ao ZIP
+                try (FileInputStream fis = new FileInputStream(arquivoparazip)) {
+                    ZipEntry zipEntry = new ZipEntry(arquivoparazip.getName());
+                    zipOut.putNextEntry(zipEntry);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = fis.read(buffer)) > 0) {
+                        zipOut.write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao compactar arquivos: " + e.getMessage());
+        }
+    }
+}
