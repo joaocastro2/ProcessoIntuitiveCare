6ebdee3b1e6e199241ef820e20b09166cdb08e66
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-29 03:49:31 -0300

    ADD: Clase que busca os dados pela url e os baixa

---
+++ TesteIntuitiveCare/WebScraping/src/main/java/com/IntuitiveCare/WebScraping/Service/WebScrapingService.java
@@ -0,0 +1,34 @@
+package com.IntuitiveCare.WebScraping.Service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class WebScrapingService {
+
+    public List<String> BuscaLink(String url) {
+        List<String> attachmentLinks = new ArrayList<>();
+        try {
+            // Conecta ao site e baixa o HTML
+            Document documento = Jsoup.connect("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos").get();
+
+            // Seleciona links que possuem "Anexo" no texto ou nome
+            Elements links = documento.select("a[href]");
+            for (Element link : links) {
+                String href = link.absUrl("href");
+                if (href.endsWith(".pdf") && (link.text().contains("Anexo I") || link.text().contains("Anexo II"))) {
+                    attachmentLinks.add(href);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao fazer web scraping: " + e.getMessage());
+        }
+        return attachmentLinks;
+    }
+}
