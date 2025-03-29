54d92a49b029525e126994d09a05bca9959c5e00
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-29 16:17:27 -0300

    ---

---
+++ TesteTecnico/ManipulaçãoPdf/src/test/java/com/TesteTecnico/ManipulacaoPdf/ManipulacaoPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TesteTecnico.ManipulacaoPdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManipulacaoPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
