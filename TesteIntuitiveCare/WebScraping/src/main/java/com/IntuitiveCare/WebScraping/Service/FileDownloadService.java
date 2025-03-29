13b1c108a137e923cab28a375f1ca1e99cbb7ddd
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-29 11:33:42 -0300

    ADD: Service para o serviço de download

---
+++ TesteIntuitiveCare/WebScraping/src/main/java/com/IntuitiveCare/WebScraping/Service/FileDownloadService.java
@@ -0,0 +1,23 @@
+package com.IntuitiveCare.WebScraping.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+@Service
+public class FileDownloadService {
+
+    public void downloadArquivo(String fileUrl, String destinationPath) throws Exception {
+        try (InputStream in = new URL(fileUrl).openStream();
+             FileOutputStream out = new FileOutputStream(destinationPath)) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
