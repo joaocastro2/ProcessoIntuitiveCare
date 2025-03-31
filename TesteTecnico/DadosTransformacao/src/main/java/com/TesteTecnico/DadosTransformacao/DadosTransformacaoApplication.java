93efa1615cfd148c7a45b2da2f0bf4aac85493ea
joaocastro2 <joaocastrocntt06@gmal.com>
2025-03-31 10:09:27 -0300

    ADD: PRIMEIRO COMMIT DESAFIO 2

---
+++ TesteTecnico/DadosTransformacao/src/main/java/com/TesteTecnico/DadosTransformacao/DadosTransformacaoApplication.java
@@ -0,0 +1,13 @@
+package com.TesteTecnico.DadosTransformacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosTransformacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosTransformacaoApplication.class, args);
+	}
+
+}
---
+++ TesteTecnico/DadosTransformacao/src/test/java/com/TesteTecnico/DadosTransformacao/DadosTransformacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TesteTecnico.DadosTransformacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosTransformacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
